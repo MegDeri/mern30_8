96184066a2212b3404f47307339d19c13b82c764
Magda Aderibigbe <magdaaderibigbe@gmail.com>
2019-09-23 22:26:15 +0300

    Updated routes in server

--- server/controllers/post.controller.js
+++ server/controllers/post.controller.js
@@ -9,4 +9,15 @@ exports.getPosts = async (req, res) => {
         res.status(500).json(err);
       }
   
-  };
+};
+
+// get single Post
+exports.getSinglePost = async (req, res) => {
+    
+  try {
+      res.status(200).json(await Post.find({id: req.params.id}));
+    } catch(err) {
+      res.status(500).json(err);
+    }
+
+};
